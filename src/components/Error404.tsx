fb55e6955a61ee7099a03fed1495869ca93d0f4d
AnnySad <anna.sadovskaya555@gmail.com>
2021-06-25 16:07:51 +0300

    ...fix 404

--- src/components/Error404.tsx
+++ src/components/Error404.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import pages404 from'./404.png'
+import s from './404.module.css'
 
 function Error404() {
     return (
-         <div>
+         <div className={s.content}>
              <div>404</div>
              <div>Page not found!</div>
              <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+             <img src={pages404}/>
 
         </div>
     )
