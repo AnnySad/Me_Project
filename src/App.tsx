bc8132a0141eb995dfb987fa4accac34c4d020a1
AnnySad <anna.sadovskaya555@gmail.com>
2021-06-25 14:54:13 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {HashRouter} from "react-router-dom";
+import Routes from './components/Routes.';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        {/*в gh-pages лучше работает HashRouter*/}
+
+        <h1>1111</h1>
+        <HashRouter>
+
+          <Header/>
+
+          <Routes/>
+
+        </HashRouter>
     </div>
   );
 }
