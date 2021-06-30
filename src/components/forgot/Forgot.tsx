48f6b0f7a19def36a93f75c477a7f558b2a936ac
olegStacevich <oleg295832611@gmail.com>
2021-06-30 12:34:28 +0200

    forgot send

--- src/components/forgot/Forgot.tsx
+++ src/components/forgot/Forgot.tsx
@@ -1,48 +1,60 @@
 import React, {ChangeEvent, useCallback, useState} from "react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {forgotThunk} from "../../bll/forgotReduser";
+import s from "./../registred/Registred.module.css"
+import {CheckEmail} from "./FogotPassword";
+import {AppStoreType} from "../../bll/store";
+import {Preloader} from "../Preloader/Preloader";
+
 
 export const Forgot = () => {
     const dispatch = useDispatch();
+    const error = useSelector<AppStoreType, string | null>((state) => state.register.error);
+    const isSend = useSelector<AppStoreType, boolean>(state => state.forgot.isSend)
+    const isFetching = useSelector<AppStoreType, boolean>((state) => state.register.isFetching);
 
-    // const  onSubmitForm = () => {
-    //     API.forgot(loginValue, emailValue).then(res => console.log(res))
-    // };
-
-    const [loginValue, setLoginValue]= useState('')
-    const [emailValue, setEmailValue]= useState('')
+    const [emailValue, setEmailValue] = useState('')
+    const [errorEmailMessage, setErrorEmailMessage] = useState<string | null>(null);
     const onSubmitForm = useCallback(() => {
-        dispatch( forgotThunk(loginValue,emailValue));
-    }, [loginValue,emailValue]);
+        dispatch(forgotThunk(emailValue));
+    }, [emailValue]);
 
-
-    const setLoginHandle = (e: ChangeEvent<HTMLInputElement>) => {
-        setLoginValue(e.currentTarget.value)
-    }
 
     const setEmailHandle = (e: ChangeEvent<HTMLInputElement>) => {
         setEmailValue(e.currentTarget.value)
     }
-
-
+    const setEmailError = () => {
+        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{1,4}$/i.test(emailValue)) {
+            setErrorEmailMessage("Invalid email address");
+        } else {
+            setErrorEmailMessage(null);
+        }
+    };
 
     return (
-        <div>
-            <p>It-incubator</p>
-            <p>Forgot your password?</p>
-            <input type='email'
-                   value={loginValue}
-                   onChange={setLoginHandle}
-                //onBlur={setEmailError}
-                   placeholder={"login"} />
-            <input type='email'
-                   value={emailValue}
-                   onChange={setEmailHandle}
-                //onBlur={setEmailError}
-                   placeholder={"Email"} />
-            <button onClick={onSubmitForm}>Send</button>
-                <p>Password: free</p>
-        </div>
-);
+        <>
+            {isFetching ? <Preloader/> : null}
+            {!isSend && <div className={s.container}>
+                <div className={s.form}>
+                    <h3>It-incubator</h3>
+                    <p>Forgot your password?</p>
+                    <input type='email'
+                           value={emailValue}
+                           onChange={setEmailHandle}
+                           className={s.inputs}
+                           onBlur={setEmailError}
+                           placeholder={"Email"}/>
+                    {errorEmailMessage && <span className={s.error}>{errorEmailMessage}</span>}
+                    <button onClick={onSubmitForm} className={s.register_btn}>Send</button>
+                    {error && <span className={s.error}>{error}</span>}
+                    <p>Did you remember your password?</p>
+                    <h4>Try logging in</h4>
+
+                </div>
+            </div>}
+            {isSend && <CheckEmail/>}
+        </>
+    );
 
 }
+
